914b0b12e0b1c18e6db3d640435f0b47488978e5
penguin92-prg <penguin92.prg@gmail.com>
2026-06-02 22:50:03 +0900

    Update

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,7 @@
 window.addEventListener("load", function(){
 
   // ナビゲーションバーの挙動設定
+
   document.querySelectorAll("nav>ul>li").forEach(li => {
     li.addEventListener("click", function(){
       document.querySelectorAll("nav>ul>li").forEach(e => {
@@ -9,7 +10,12 @@ window.addEventListener("load", function(){
       this.dataset.state = "current";
       document.getElementById("main-carousel").dataset.tab = this.dataset.tab;
     });
+    if(li.dataset.state == "current"){
+      li.click();
+    }
   });
 
-
+  document.getElementById("quiz-card").addEventListener("click", function(){
+    this.classList.toggle("flipped")
+  })
 });
